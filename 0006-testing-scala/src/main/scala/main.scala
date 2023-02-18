52de77114d4461d9c895914df287d1dc9bfa3b5e
cogdiver <valentina.lozano@jikkosoft.com>
2023-02-18 17:11:37 -0500

    codigo base testing en scala

---
+++ 0006-testing-scala/src/main/scala/main.scala
@@ -0,0 +1,8 @@
+import operations.sum._
+
+object Main {
+    def main(args: Array[String]) = {
+        println("Hello World!")
+        println(sum2(2))
+    }
+}
---
+++ 0006-testing-scala/src/main/scala/operations/sum.scala
@@ -0,0 +1,16 @@
+package operations
+
+/**
+  * package que hace diferentes sumas
+  */
+object sum {
+
+    /** suma 2 a un valor
+      * @param value: Int
+      * @return value + 2
+      */
+    def sum2(value: Int) = value + 2
+    def sum3(value: Int) = value + 3
+    def sum4(value: Int) = value + 4
+
+}
